8a4f32000834aa493f1eed63812fbe14da6bd977
ctyjiuh <56714018+ctyjiuh@users.noreply.github.com>
2021-11-23 08:32:38 +0300

    Create veeam_jobbefore.bat

---
+++ veeam_jobbefore.bat
@@ -0,0 +1,6 @@
+rem Run this script at job start:
+rem 1. Edit Veeam Job
+rem 2. Storage section - Advanced
+rem 3. Scripts tab - Run following script before the job
+rem 4. Browse - choose this script file
+powershell C:\dt\script\veeam_jobbefore.ps1
